ad2205b031b73454e22037a96b41f5152ab33eeb
soufianefariss <soufiane.fariss@um5s.net.ma>
2020-10-03 22:38:24 +0100

    Freed used dynamically allocated memroy

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "lib/bruter.h"
 
@@ -8,10 +9,13 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
-  /* Populate host */
+  /* Check if file exits and user has read permissions */
   if (check_access(argv[2])) return 0;
 
-  create_hosts_list(argv[1], argv[2]);
+  /* Create a list of hosts from the input host */
+  char** hosts_list = create_hosts_list(argv[1], argv[2]);
+  for (unsigned int i = 0; i < count_lines(argv[2]); i++) free(hosts_list[i]);
+  free(hosts_list);
   return 0;
 }
 
